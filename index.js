4c8c6bfac1ef7fa27594d84817a080bea45eb5a9
chetan panara <chetanpanara88@gmail.com>
2024-11-30 13:36:04 +0530

    commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect(process.env.connection)
+mongoose.connect(process.env.CONNECTION)
 
 // mongoose.connect('mongodb://localhost:27017/mywebsite')
 
